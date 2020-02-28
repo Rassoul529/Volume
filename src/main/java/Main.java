5ddd850e8eccc79c77622e4095020f8e26664656
Rassoul529 <wamadou@aol.com>
2020-02-27 16:41:51 -0800

    set up

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        int boxArea = 3;
+        int height = 3;
+        int Volume = boxArea * height;
+        System.out.println(Volume);
+    }
+}
